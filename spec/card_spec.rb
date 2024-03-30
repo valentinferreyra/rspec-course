ea0acd33268ec6d36d2e8d7278bd18d4182cf0df
Valentin Ferreyra <valen.ferreyra@outlook.com>
2024-03-30 00:01:59 -0300

    rspec examples

---
+++ spec/card_spec.rb
@@ -0,0 +1,31 @@
+class Card 
+    attr_accessor :rank, :suit
+    def initialize(rank, suit)
+        @rank = rank
+        @suit = suit
+    end
+end
+
+RSpec.describe Card do
+    let(:card) { Card.new('Ace', 'Spades') }
+
+    # before do
+    #     @card = Card.new('Ace', 'Spades')
+    # end
+
+    it 'has a rank and that rank can change' do
+        expect(card.rank).to eq('Ace')
+        card.rank = 'Queen'
+        expect(card.rank).to eq('Queen')
+    end
+
+    it 'has a suit' do
+        expect(card.suit).to eq('Spades')
+    end
+
+    it 'has a custom error message' do
+        card.suit = 'Nonsense'
+        comparison = 'Spades'
+        expect(card.suit).to eq(comparison), "Expected #{comparison} but got #{card.suit}"
+    end
+end
